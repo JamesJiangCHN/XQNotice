19a514dacc180569145640a83ba7fcca8b2ff1a2
JamesJiangCHN <JamesJiangCHN@gmail.com>
2015-08-21 18:10:07 +0800

    自定义组合（开发中）

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 ﻿
-var count = 0;
+var count;
 
 function getLocalTime(timeMs) {     
     return new Date(parseInt(timeMs)).toLocaleString()
@@ -31,4 +31,30 @@ function check(){
 
 $(document).ready(function(){
 	setInterval(check, 5000);  
+	
+	var MaxInputs       = 8; //maximum input boxes allowed  
+	var InputsWrapper   = $("#InputsWrapper"); //Input boxes wrapper ID  
+	var AddButton       = $("#AddMoreFileBox"); //Add button ID  
+	  
+	var x = InputsWrapper.length; //initlal text box count  
+	var FieldCount=1; //to keep track of text box added  
+	  
+	$(AddButton).click(function (e){  //on add input button click  
+			if(x <= MaxInputs) //max input box allowed  
+			{  
+				FieldCount++; //text box added increment  
+				//add input box  
+				$(InputsWrapper).append('<div style="margin:10px 0"><input type="text" name="mytext[]" id="field_'+ FieldCount +'" value="Text '+ FieldCount +'"/><a href="#" class="removeclass">×</a></div>');  
+				x++; //text box increment  
+			}  
+	return false;  
+	});  
+	  
+	$("body").on("click",".removeclass", function(e){ //user click on remove text  
+			if( x > 1 ) {  
+					$(this).parent('div').remove(); //remove text box  
+					x--; //decrement textbox  
+			}  
+	return false;  
+	})   
 });
