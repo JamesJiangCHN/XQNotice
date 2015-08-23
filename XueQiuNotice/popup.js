08ae88ba1b99acf177be5dbfb6d34727b7f247a1
JamesJiang <jamesjiangchn@gmail.com>
2015-08-23 21:58:30 +0800

    修正添加删除后序号错误的问题

--- XueQiuNotice/popup.js
+++ XueQiuNotice/popup.js
@@ -1,45 +1,53 @@
 ﻿
 regZh=/^ZH\d{6}$/;  
-function getZhDetail(id, zhCode)
+function getZhDetail(id, zhSymbol)
 {
-    $.get("http://xueqiu.com/cubes/quote.json?code="+zhCode+"&return_hasexist=false&_="+new Date().getTime(),		
+    $.get("http://xueqiu.com/cubes/quote.json?code="+zhSymbol+"&return_hasexist=false&_="+new Date().getTime(),		
         function(data){
             if(data.error_description == null){
                 $("#"+id).next("a").remove();
-                $("#"+id).after('<a href="http://xueqiu.com/P/'+zhCode+'" target="_blank">'+data[zhCode].name+'</a>');
-                //chrome.extension.sendMessage({cmd: "notify",type:"basic", mesg:data[zhCode].name},function(response) {});
+                var zhName = data[zhSymbol].name
+                if (zhName.length > 5) { 
+                    zhName = zhName.substring(0,5)+"…"
+                }
+                $("#"+id).after('<a href="http://xueqiu.com/P/'+zhSymbol+'" target="_blank">'+zhName+'</a>');
+                //chrome.extension.sendMessage({cmd: "notify",type:"basic", mesg:data[zhSymbol].name},function(response) {});
             }
             else{
                 $("#"+id).next("a").remove();
                 $("#"+id).after('<a href="#">不存在</a>');
-                //chrome.extension.sendMessage({cmd: "notify",type:"basic", mesg:"组合 "+zhCode+" 不存在"},function(response) {});
+                //chrome.extension.sendMessage({cmd: "notify",type:"basic", mesg:"组合 "+zhSymbol+" 不存在"},function(response) {});
             }
     }).fail(function() {
         $("#"+id).next("a").remove();
         $("#"+id).after('<a href="#">不存在</a>');
-        //chrome.extension.sendMessage({cmd: "notify",type:"basic", mesg:"组合 "+zhCode+" 不存在"},function(response) {});
+        //chrome.extension.sendMessage({cmd: "notify",type:"basic", mesg:"组合 "+zhSymbol+" 不存在"},function(response) {});
   });	
 }
 
-function checkZhDetail(index, zhCode)
+function checkZhDetail(id, zhSymbol)
 {
-    $.get("http://xueqiu.com/cubes/quote.json?code="+zhCode+"&return_hasexist=false&_="+new Date().getTime(),		
+    $.get("http://xueqiu.com/cubes/quote.json?code="+zhSymbol+"&return_hasexist=false&_="+new Date().getTime(),		
         function(data){
             if(data.error_description == null){
-                $("#zh_"+index).next("a").remove();
-                $("#zh_"+index).after('<a href="http://xueqiu.com/P/'+zhCode+'" target="_blank">'+data[zhCode].name+'</a>');
-                window.localStorage.setItem(index,zhCode);  
-                //chrome.extension.sendMessage({cmd: "notify",type:"basic", mesg:data[zhCode].name},function(response) {});
+                $("#"+id).next("a").remove();
+                var zhName = data[zhSymbol].name
+                if (zhName.length > 5) { 
+                    zhName = zhName.substring(0,5)+"…"
+                }
+                $("#"+id).after('<a href="http://xueqiu.com/P/'+zhSymbol+'" target="_blank">'+zhName+'</a>');
+                window.localStorage.setItem(zhSymbol,zhName);  
+                //chrome.extension.sendMessage({cmd: "notify",type:"basic", mesg:data[zhSymbol].name},function(response) {});
             }
             else{
-                $("#zh_"+index).next("a").remove();
-                $("#zh_"+index).after('<a href="#">不存在</a>');
-                //chrome.extension.sendMessage({cmd: "notify",type:"basic", mesg:"组合 "+zhCode+" 不存在"},function(response) {});
+                $("#"+id).next("a").remove();
+                $("#"+id).after('<a href="#">组合不存在</a>');
+                //chrome.extension.sendMessage({cmd: "notify",type:"basic", mesg:"组合 "+zhSymbol+" 不存在"},function(response) {});
             }
     }).fail(function() {
-        $("#zh_"+index).next("a").remove();
-        $("#zh_"+index).after('<a href="#">不存在</a>');
-        //chrome.extension.sendMessage({cmd: "notify",type:"basic", mesg:"组合 "+zhCode+" 不存在"},function(response) {});
+        $("#"+id).next("a").remove();
+        $("#"+id).after('<a href="#">组合不存在</a>');
+        //chrome.extension.sendMessage({cmd: "notify",type:"basic", mesg:"组合 "+zhSymbol+" 不存在"},function(response) {});
   });	
 }
 
@@ -50,8 +58,8 @@ $(document).ready(function(){
 	var InputsWrapper   = $("#InputsWrapper"); //Input boxes wrapper ID  
 	var AddButton       = $("#AddMoreFileBox"); //Add button ID  
 	var SaveButton      = $("#SaveBtn");
-	var x = 0; //initlal text box count  
-	var FieldCount=1; //to keep track of text box added  
+	var x = 0;          //initlal text box count  
+	var sumCount=1;     //to keep track of text box added  
     
     //alert(window.localStorage.length);
     if(window.localStorage.length < 1){
@@ -60,12 +68,13 @@ $(document).ready(function(){
     }
     else{
         for(var i=0,len=window.localStorage.length;i<len;i++){
-            zhKey=window.localStorage.key(i);
-            zhValue=window.localStorage.getItem(zhKey)
+            zhSymbol=window.localStorage.key(i);
+            zhName=window.localStorage.getItem(zhSymbol)
             
-            $(InputsWrapper).append('<div style="margin:10px 0"><a href="#" class="removeclass">×</a><input type="text" style="width:128px;" class="zhItem"  id="zh_'+ zhKey +'" value="'+zhValue+'"/></div>'); 
-            getZhDetail("zh_"+ zhKey, zhValue)
-            x++;            
+            $(InputsWrapper).append('<div style="margin:10px 0"><a href="#" class="removeclass">×</a><input type="text" style="width:128px;" class="zhItem"  id="zh_'+ i +'" value="'+zhSymbol+'"/><a href="http://xueqiu.com/P/'+zhSymbol+'" target="_blank">'+zhName+'</a></div>'); 
+            //getZhDetail("zh_"+ zhKey, zhValue)
+            x++;  
+            sumCount++; //text box added increment              
         }
 	}  
     
@@ -82,9 +91,9 @@ $(document).ready(function(){
 			{  
 
 				//add input box  
-				$(InputsWrapper).append('<div style="margin:10px 0"><a href="#" class="removeclass">×</a><input type="text" style="width:128px;" class="zhItem"  id="zh_'+ FieldCount +'" value=""/></div>');  
+				$(InputsWrapper).append('<div style="margin:10px 0"><a href="#" class="removeclass">×</a><input type="text" style="width:128px;" class="zhItem"  id="zh_'+ sumCount +'" value=""/></div>');  
 				x++; //text box increment  
-                FieldCount++; //text box added increment  
+                sumCount++; //text box added increment  
 			}  
             return false;  
 	});  
@@ -98,19 +107,16 @@ $(document).ready(function(){
             var zhSymbol = element.value.trim().toUpperCase();
             if(zhSymbol.length > 0){
                 if(regZh.test(zhSymbol)){
-                   checkZhDetail(index, zhSymbol);
+                   checkZhDetail(element.id, zhSymbol);
                 }
                 else{
                     errzh +=element.value+" "
-                    $("#"+id).next("a").remove()
-                    $(this).after('<a href="#">错误</a>');
+                    $(this).next("a").remove()
+                    $(this).after('<a href="#">输入错误</a>');
                 }
             }
             
         })
-        if(errzh !="" ){
-            alert("组合代码："+errzh+",输入错误！")
-        }
         //$.each($(".zhItem"), function(i,val){  
         //    window.localStorage.setItem(index,element.value);  
         //});  
